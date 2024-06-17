d43c6e7af2732b2df4fa3d2083ac9797b5756a30
echo <echo@echo.com>
2024-06-17 16:07:25 +0800

    vite config, eslint, husky, prettier

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  let env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '~': path.resolve(__dirname, './')
+      }
+    },
+    server: {
+      port: 3001,
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_API,
+          changeOrigin: true,
+          cookieDomainRewrite: '',
+          secure: false
+        }
+      }
+    }
+  })
+}

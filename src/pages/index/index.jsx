1e6256b9ada540bac5fc6c1f1061c4e0909a0437
echo <echo@echo.com>
2024-06-17 20:49:32 +0800

    1

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,12 +1,15 @@
 import './index.scss'
+import avatar1 from '@/assets/images/avatars/avatar-1.png'
 
 const Index = () => {
   return (
-    <div>
-      <span>
-        Index
-      </span>
-      Page
+    <div className="ape-ben-home-page">
+      <img
+        src={avatar1}
+        alt="This is a image"/>
+      <img
+        src="https://png.pngtree.com/png-clipart/20220404/original/pngtree-d-rendering-male-character-profile-or-avatar-happy-young-man-with-png-image_7512919.png"
+        alt="This is a image"/>
     </div>
   )
 }
